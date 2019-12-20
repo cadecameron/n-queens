c45340017a1923ac90af4457cc47d8080cbee39b
cadecameron <cade.cameron@gmail.com>
2019-12-19 17:45:15 -0800

    Refactor solvers.js to have externally defined search function

--- src/solvers.js
+++ src/solvers.js
@@ -15,115 +15,40 @@
 
 
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined;
+window.findNRooksSolution = function (n) {
+  solution = undefined;
   // create a new Board instance using {n: n}
-  var testBoard = new Board({n: n});
-
-  // define an inner function to recursively call for each successful piece placement, that takes a single parameter 'row'
-  var placeAndCheckSearch = function(row) {
-    // iterate through the current row's column elements while solution = undefined
-    for (var column = 0; column < n && solution === undefined; column++) {
-      // check if the current row equals n. If it does, that means a valid Board was found (set solution = Board)
-      if (row === n) {
-        solution = _.filter(testBoard.attributes, (value, key) => !isNaN(key));
-      } else {
-        // place a piece at the current colum in the row
-        testBoard.get(row)[column] = 1;
-
-        // check if there's a conflict in the current Board
-        if (testBoard.hasAnyRooksConflicts()) {
-          // if there is a conflict
-          // remove last piece from Board
-          testBoard.get(row)[column] = 0;
-        } else {
-          // if no conflict
-          // recursively call inner function, passing in row + 1
-          placeAndCheckSearch(row + 1);
-        }
-      }
-    }
-  };
+  testBoard = new Board({ n: n });
 
   // call inner function with row = 0
-  placeAndCheckSearch(0);
+  placeAndCheckSearch(0, 'rooks', '1');
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = 0;
+window.countNRooksSolutions = function (n) {
+  solutionCount = 0;
   // create a new Board instance using {n: n}
-  var testBoard = new Board({n: n});
+  testBoard = new Board({ n: n });
 
-  // define an inner function to recursively call for each successful piece placement, that takes a single parameter 'row'
-  var placeAndCheckSearch = function(row) {
-    // iterate through the current row's column elements while solution = undefined
-    for (var column = 0; column < n; column++) {
+  placeAndCheckSearch(0, 'rooks', 'all');
 
-      // place a piece at the current colum in the row
-      testBoard.get(row)[column] = 1;
-      var conflict = testBoard.hasAnyRooksConflicts();
-
-      // check if there's a conflict in the current Board
-      if (!conflict && row < n - 1) {
-        // if no conflict and not on last row
-        // recursively call inner function, passing in row + 1
-        placeAndCheckSearch(row + 1);
-      } else if (!conflict) {
-        // no conflict AND last row, means its a solution
-        solutionCount++;
-      }
-      // remove last piece from Board before continuing row iteration
-      testBoard.get(row)[column] = 0;
-    }
-  };
-
-  // call inner function with row = 0
-  placeAndCheckSearch(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n) {
-  var solution;
-  var solutionFound = false;
+window.findNQueensSolution = function (n) {
+  solution = undefined;
 
   // create a new Board instance using {n: n}
-  var testBoard = new Board({n: n});
-
-  // define an inner function to recursively call for each successful piece placement, that takes a single parameter 'row'
-  var placeAndCheckSearch = function(row) {
-    // iterate through the current row's column elements while solution = undefined
-    for (var column = 0; column < n && !solutionFound; column++) {
-
-      // place a piece at the current colum in the row
-      testBoard.get(row)[column] = 1;
-      var conflict = testBoard.hasAnyQueensConflicts();
-
-      // check if there's a conflict in the current Board
-      if (!conflict && row < n - 1) {
-        // if no conflict and not on last row
-        // recursively call inner function, passing in row + 1
-        placeAndCheckSearch(row + 1);
-      } else if (!conflict) {
-        // no conflict AND last row, means its a solution
-        solution = _.filter(testBoard.attributes, (value, key) => !isNaN(key));
-        solutionFound = true;
-      }
-      // remove last piece from Board before continuing row iteration
-      if (!solutionFound) {
-        testBoard.get(row)[column] = 0;
-      }
-    }
-  };
+  testBoard = new Board({ n: n });
 
   // call inner function with row = 0
-  placeAndCheckSearch(0);
-  if (!solutionFound) {
+  placeAndCheckSearch(0, 'queen', '1');
+  if (solution === undefined) {
     solution = _.filter(testBoard.attributes, (value, key) => !isNaN(key));
   }
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
@@ -131,40 +56,56 @@ window.findNQueensSolution = function(n) {
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function(n) {
+window.countNQueensSolutions = function (n) {
   if (n === 0) {
     return 1;
   }
-  var solutionCount = 0;
+  solutionCount = 0;
+
   // create a new Board instance using {n: n}
-  var testBoard = new Board({n: n});
-
-  // define an inner function to recursively call for each successful piece placement, that takes a single parameter 'row'
-  var placeAndCheckSearch = function(row) {
-    // iterate through the current row's column elements while solution = undefined
-    for (var column = 0; column < n; column++) {
-
-      // place a piece at the current colum in the row
-      testBoard.get(row)[column] = 1;
-      var conflict = testBoard.hasAnyQueensConflicts();
-
-      // check if there's a conflict in the current Board
-      if (!conflict && row < n - 1) {
-        // if no conflict and not on last row
-        // recursively call inner function, passing in row + 1
-        placeAndCheckSearch(row + 1);
-      } else if (!conflict) {
-        // no conflict AND last row, means its a solution
-        solutionCount++;
-      }
-      // remove last piece from Board before continuing row iteration
-      testBoard.get(row)[column] = 0;
-    }
-  };
+  testBoard = new Board({ n: n });
 
   // call inner function with row = 0
-  placeAndCheckSearch(0);
+  placeAndCheckSearch(0, 'queen', 'all');
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
+};
+
+// DEFINE GLOBAL VARIABLES
+var solutionCount;
+var solution;
+var testBoard;
+
+var placeAndCheckSearch = function (row, piece, returnType) {
+// determine which search mode (1/all)
+
+  // iterate through the current row's column elements while solution = undefined
+  for (var column = 0; column < testBoard.get('n') && (returnType === 'all' || solution === undefined); column++) {
+
+    // place a piece at the current colum in the row
+    testBoard.get(row)[column] = 1;
+
+    // test for conflicts based on piece
+    var conflict = piece === 'queen' ? testBoard.hasAnyQueensConflicts() : testBoard.hasAnyRooksConflicts();
+
+    // check if there's a conflict in the current Board
+    if (!conflict && row < testBoard.get('n') - 1) {
+      // if no conflict and not on last row
+      // recursively call inner function, passing in row + 1
+      placeAndCheckSearch(row + 1, piece, returnType);
+    } else if (!conflict) {
+      // no conflict AND last row, means its a solution
+      // update correct global solution varibale based on number of solutions required
+      if (returnType === 'all') {
+        solutionCount++;
+      } else {
+        solution = _.filter(testBoard.attributes, (value, key) => !isNaN(key));
+      }
+    }
+    // remove last piece from Board before continuing row iteration
+    if (returnType === 'all' || solution === undefined) {
+      testBoard.get(row)[column] = 0;
+    }
+  }
 };
